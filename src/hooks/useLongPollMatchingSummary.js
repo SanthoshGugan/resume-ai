083d00b9d025d8bd3df0b1d5a29e7099a1161d1b
Santhosh Gugan <santhoshg199@gmail.com>
2024-08-23 10:25:22 +0530

    jdSummary and match summary added

--- src/hooks/useLongPollMatchingSummary.js
+++ src/hooks/useLongPollMatchingSummary.js
@@ -1,6 +1,31 @@
 import React, { useState } from "react";
 import { pollSummaryApi } from "../api/matchingApi";
 
+export const MOCK_QUERY_SUMMARY = {
+    topResume: {
+        sentence: "The resume [single-page.pdf_resume-assets-008971676609] is a top match with the job description, showing a similarity of 80.77%",
+        resume_id: "[single-page.pdf_resume-assets-008971676609",
+        matchPercent: 80.77
+    },
+    topListResume: {
+        sentence: "Here is the top 2 resumes matching job description. \n1. Resume [single-page.pdf_resume-assets-008971676609] matches 80.77% \n2. Resume [Profile_2.pdf_resume-assets-008971676609] matches 72.66%",
+        list: [
+            {
+                resume_id: "single-page.pdf_resume-assets-008971676609",
+                matchPercent: 80.77
+            }
+        ]
+    },
+    groupByLabels: {
+        full_stack:[
+            "Get_Started_With_Smallpdf-output.pdf_resume-assets-008971676609",
+            "Profile.pdf_resume-assets-008971676609",
+            "Profile_2.pdf_resume-assets-008971676609",
+            "single-page.pdf_resume-assets-008971676609"
+        ]
+    }
+}
+
 const useLongPollMatchingSummary = ({ interval = 5000 }) => {
     const [ retry, setRetry ] = useState(false);
     const [ summary, setSummary ] = useState("");
@@ -19,7 +44,7 @@ const useLongPollMatchingSummary = ({ interval = 5000 }) => {
             return;
         }
         setRetry(retry);
-        setSummary(summary);
+        setSummary(JSON.parse(summary));
     }
 
     return {
