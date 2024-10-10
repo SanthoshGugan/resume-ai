7fe29deddc4ef1d3abcfd39e294a5700f7b1dd47
Santhosh Gugan <santhoshg199@gmail.com>
2024-10-10 17:15:41 +0530

    usermflag feature service integration. added to resume upload limit

--- src/store/thunks/userThunk.js
+++ src/store/thunks/userThunk.js
@@ -1,6 +1,7 @@
 import { signOut } from 'aws-amplify/auth';
 import { resetStore } from './commonThunk';
-import { setuserId } from '../userSlice';
+import { setFlags, setLoadingFlags, setuserId } from '../userSlice';
+import { fetchUserFeature } from '../../api/userApi';
 
 export const userLogout = (navigate) => async  (dispatch, getState) => {
     signOut();
@@ -8,3 +9,21 @@ export const userLogout = (navigate) => async  (dispatch, getState) => {
     dispatch(setuserId(null));
     navigate('/login'); 
 };
+
+export const userSync = (id) => async (dispatch, getState) => {
+    if (!id) return;
+    try {
+        dispatch(setLoadingFlags(true));
+        const res = await fetchUserFeature({ Key: { id } });
+        const {user: user_feature} = res?.data;
+        const { plan = {} } = user_feature || {};
+        const { flags = {} } = plan;
+        dispatch(setFlags(flags));
+    } catch (err) {
+        console.error(`error while fetching user features`, err);
+        dispatch(setFlags({}));
+    } finally {
+        dispatch(setuserId(id));
+        dispatch(setLoadingFlags(false));
+    }
+}
